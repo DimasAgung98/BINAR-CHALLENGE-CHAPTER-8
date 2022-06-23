7427eeb910cf251ff1d4a1627b65bca740159dfd
dimas agung <dimasraden0798@gmail.com>
2022-06-23 21:34:43 +0800

    add swagger for all API

--- server/config/config.js
+++ server/config/config.js
@@ -2,23 +2,23 @@ require('dotenv').config()
 module.exports = {
   "development": {
     "username": process.env.DB_USERNAME_DEV || "postgres",
-    "password": process.env.DB_PASSWORD_DEV || "postgres",
+    "password": process.env.DB_PASSWORD_DEV || "Munafuck98",
     "database": process.env.DB_DEV || "c8-dev",
-    "host": "127.0.0.1",
+    "host": "localhost",
     "dialect": "postgres"
   },
   "test": {
     "username": process.env.DB_USERNAME_TEST,
     "password": process.env.DB_PASSWORD_TEST,
     "database": process.env.DB_TEST,
-    "host": "127.0.0.1",
+    "host": "localhost",
     "dialect": "postgres"
   },
   "production": {
     "username": process.env.DB_USERNAME_PROD,
     "password": process.env.DB_PASSWORD_PROD,
     "database": process.env.DB_PROD,
-    "host": "127.0.0.1",
+    "host": "localhost",
     "dialect": "postgres"
   }
 }
